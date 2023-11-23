3a83c2b3b244cd7b73545a6715e71767c46f9bd2
jgooday48 <joelgooday48@gmail.com>
2023-11-23 09:33:19 +0000

    add front end html

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -14,6 +14,7 @@ CREATE TABLE user_account (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(30) UNIQUE NOT NULL,
     password CHAR(60) NOT NULL,
+    isAdmin BOOLEAN DEFAULT FALSE;
     PRIMARY KEY (user_id)
 );
 
