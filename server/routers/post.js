06aae09448d8b2a399a030fbdcd609c058452913
jgooday48 <joelgooday48@gmail.com>
2023-11-23 11:00:38 +0000

    add update post content to backend

--- server/routers/post.js
+++ server/routers/post.js
@@ -9,6 +9,7 @@ postRouter.get("/", authenticator, postController.index);
 postRouter.get("/", postController.index);
 postRouter.post("/", postController.create);
 postRouter.get("/:id", postController.show);
+postRouter.patch("/id",postController.update);
 postRouter.delete("/:id", postController.destroy);
 
 module.exports = postRouter;
