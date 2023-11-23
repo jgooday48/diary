89f1c3412f463a6dcc20e18f9c4155f8f9f826bf
asiek728 <asiek728@gmail.com>
2023-11-23 11:15:24 +0000

    add edit/remove buttons for each post

--- server/routers/post.js
+++ server/routers/post.js
@@ -5,7 +5,8 @@ const postController = require('../controllers/post.js');
 
 const postRouter = Router();
 
-postRouter.get("/", authenticator, postController.index);
+//postRouter.get("/", authenticator, postController.index);
+postRouter.get("/", postController.index);
 postRouter.get("/", postController.index);
 postRouter.post("/", postController.create);
 postRouter.get("/:id", postController.show);
