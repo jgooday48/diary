e371e9a9ad119c525f1368b7f01e67ad6eb5d969
asiek728 <asiek728@gmail.com>
2023-11-23 16:25:07 +0000

    add edit post in front-end

--- client/assets/js/diary.js
+++ client/assets/js/diary.js
@@ -1,26 +1,25 @@
-function createPostElement (data) {
+function createPostElement(data) {
     const post = document.createElement("div");
     post.className = "post";
 
-    
     const date = document.createElement("p");
     date.textContent = data["date"];
     post.appendChild(date);
-    
+
     const header = document.createElement("h1");
     header.textContent = data["title"];
     post.appendChild(header);
-    
+
     const category = document.createElement("h3");
     category.textContent = data["category"];
     post.appendChild(category);
-    
-    
-    
+
     const content = document.createElement("p");
     content.textContent = data["content"];
+    content.setAttribute("contenteditable", "true")
+
     post.appendChild(content);
-    
+
     const removeBtn = document.createElement("button")
     removeBtn.className = "removeBtn";
     removeBtn.textContent = "remove"
@@ -28,24 +27,24 @@ function createPostElement (data) {
 
     removeBtn.addEventListener('click', async () => {
         const options = {
-          headers: {
-            Authorization: localStorage.getItem('token')
-          },
-          method: 'DELETE'
+            headers: {
+                Authorization: localStorage.getItem('token')
+            },
+            method: 'DELETE'
         };
-    
+
         const response = await fetch(
-          `http://localhost:3000/posts/${data['id']}`,
-          options
+            `http://localhost:3000/posts/${data['id']}`,
+            options
         );
-    
+
         if (response.status === 204) {
-          window.location.reload();
+            window.location.reload();
         } else {
-          const respData = await response.json();
-          alert(respData.error);
+            const respData = await response.json();
+            alert(respData.error);
         }
-      });
+    });
 
 
 
@@ -54,6 +53,32 @@ function createPostElement (data) {
     const editBtn = document.createElement("button")
     editBtn.className = "removeBtn";
     editBtn.textContent = "edit"
+
+    editBtn.addEventListener('click', async () => {
+        console.log(content.textContent)
+
+
+        const options = {
+            method: "PATCH",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: content.textContent
+            })
+        }
+
+        const response = await fetch(
+            `http://localhost:3000/posts/${data['id']}`,
+            options
+        );
+
+
+    });
+
+
+
     post.appendChild(editBtn)
     return post;
 }
@@ -83,10 +108,10 @@ document.getElementById("post-form").addEventListener("submit", async (e) => {
     }
 })
 
-async function loadPosts () {
-    
+async function loadPosts() {
+
     // client/assets/board.js
-// loadPosts function
+    // loadPosts function
     const options = {
         headers: {
             'Authorization': localStorage.getItem("token")
@@ -96,7 +121,7 @@ async function loadPosts () {
 
     if (response.status == 200) {
         const posts = await response.json();
-    
+
         const container = document.getElementById("posts");
 
         posts.forEach(p => {
