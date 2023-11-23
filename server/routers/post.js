272e3d53fbde927fb4f964eda1bbbd9f0a81372a
jgooday48 <joelgooday48@gmail.com>
2023-11-23 15:58:48 +0000

    increase size of content text area

--- server/routers/post.js
+++ server/routers/post.js
@@ -6,7 +6,7 @@ const postController = require('../controllers/post.js');
 const postRouter = Router();
 
 postRouter.get("/", authenticator, postController.index);
-postRouter.get("/", postController.index);
+// postRouter.get("/", postController.index);
 postRouter.post("/", postController.create);
 postRouter.get("/:id", postController.show);
 postRouter.patch("/:id",postController.update);
